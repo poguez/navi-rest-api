b11cf31ecac3020e45ee3fe953ba7397605e0745
poguez <zeugop@gmail.com>
2017-06-06 18:43:57 +0900

    Postgis 4 added csv support

--- import_conflict_points_from_csv.py
+++ import_conflict_points_from_csv.py
@@ -1,13 +1,18 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from models_postgis import ConflictPoint
+import csv
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/navi_voice_assistant', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-lake = ConflictPoint(borough='Manhattan', risk=12, geom='POINT(-73.9745 40.7585)')
-session.add(lake)
-session.commit()
-
+with open('risk_points.csv', 'rb') as csvfile:
+    risk_points_reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    for row in risk_points_reader:
+        # print ', '.join(row)
+        geometry_value = "POINT(%s %s)" % (row['latitude'], row['longitude'])
+        risk_value = int(row['risk'])
+        lake = ConflictPoint(borough='Manhattan', risk=risk_value, geom=geometry_value)
+        session.add(lake)
+        session.commit()
