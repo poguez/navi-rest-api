7c5a17a161b696eaafab05390841730ec9e21865
poguez <zeugop@gmail.com>
2017-06-06 18:11:06 +0900

    Postgis 4

--- import_conflict_points_from_csv.py
+++ import_conflict_points_from_csv.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from models_postgis import ConflictPoint
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/gis', echo=True)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/navi_voice_assistant', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
